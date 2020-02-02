0182f2b8941952e11926a8ab32003ebaf645a7c0
Todd Hendricks <hendricks.ta@gmail.com>
2020-02-02 13:36:46 -0800

    refactor first condition that addresses the input with numeric characters at the end. increment successful.

--- codewars.py
+++ codewars.py
@@ -1,7 +1,12 @@
 # kata: https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/python
 def increment_string(string: str) -> str:
     string_list = list(string)
-    integers = list(filter(lambda s: s.isnumeric(),string_list))
-    single_int = int("".join(map(str, integers)))
-    print(single_int)
-increment_string("Ju23456ice")
+    if string_list[-1].isnumeric:
+        integers = list(filter(lambda s: s.isnumeric(),string_list))
+        list_to_int = ''.join([str(elem) for elem in integers])
+        plus_one = int(list_to_int) + 1
+        print(plus_one) 
+    else:
+        string_list[-1] = 1
+        print(str(string_list))
+increment_string("Juice55")
