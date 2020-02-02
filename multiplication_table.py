91fd0dae655c7d800acef7d45ca45f9c9d1170a8
Todd Hendricks <hendricks.ta@gmail.com>
2020-02-01 20:27:23 -0800

    functional but wrong. very rusty with python right now

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,10 +1,22 @@
-def multiplication_table(row: int,column: int) -> list:
-    x = 0
+# def multiplication_table(row: int,column: int) -> list:
+#     scalar = 1
+#     for i in range(len(column)):
+#         row = list(map(lambda x: x * scalar, column))
+#         scalar = scalar + 1
+#         print(row)
+
+# multiplication_table(5,4)
+
+def tester(n):
+    z = range(n)
+    inc = 1
     foo = []
-    for row in range(row):
-        x = x + 1
-        foo.append(x)
+    for i in z:
+        boo = list(map(lambda x: x * inc, z))
+        foo.append(boo)
+        inc = inc + 1
+    for a in foo:
+        a.remove(0)
     print(foo)
 
-
-multiplication_table(5,4)
+tester(8)
