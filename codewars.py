c4833e921c6b9f1d5ceb5af53a9f9627ebbf85c2
Todd Hendricks <hendricks.ta@gmail.com>
2020-02-03 06:25:04 -0800

    working solution that passed the test suite

--- codewars.py
+++ codewars.py
@@ -15,6 +15,12 @@
 # kata: https://www.codewars.com/kata/56170e844da7c6f647000063/train/python
 def people_with_age_drink(age):
     assert isinstance(age, (int, float)) and not isinstance(age, bool)
-    print(age)
-
+    if age < 14:
+        return "drink toddy"
+    elif age >= 14 and age < 18:
+        return "drink coke"
+    elif age >= 18 and age < 21:
+        return "drink beer"
+    elif age >= 21:
+        return "drink whisky"
 people_with_age_drink(5)
