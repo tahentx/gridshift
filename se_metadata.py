33d9b731378b3cb808ac5363fd25d38fdf69a856
Todd Hendricks <hendricks.ta@gmail.com>
2020-02-06 10:41:14 -0800

    convert nrel json to named tuple

--- se_metadata.py
+++ se_metadata.py
@@ -1,4 +1,8 @@
 import json
+from collections import namedtuple
 path = '/Users/toddhendricks/Public/code/gridshift/metdata.json'
 with open(path, 'r') as j:
      contents = json.loads(j.read())
+dataset = namedtuple('dataSet', ['Access','Code', 'modified'])
+tucson = dataset(contents['accessLevel'],contents['bureauCode'],contents['modified'])
+print(tucson)
