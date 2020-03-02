9e9a11e5c3f119e31e56776e92fd7e6f4e2df666
Todd Hendricks <hendricks.ta@gmail.com>
2020-03-01 22:14:39 -0800

    first steps at alphabetized kata.

--- metis.py
+++ metis.py
@@ -72,3 +72,59 @@ def max_gap(numbers):
 #     lst = sorted(numbers)
 #     return max(b-a for a,b in zip(lst, lst[1:]))
 # max_gap([3,5,9])
+
+def string_reverse_A(input_str):
+    input_str = input_str[::-1]
+    a_cont = []
+    for letter in input_str:
+        a_cont.append(letter)
+        a_cont.append("A")
+    a_string = ''.join(a_cont)
+    return a_string
+string_reverse_A("hello")
+
+def tweener(lower,upper):
+    values = range(lower,upper + 1)
+    output = []
+    for x in values:
+        if x % 2 == 0:
+            output.append(x)
+        elif x % 7 == 0:
+            output.append(x)
+        else:
+            pass
+    return output
+tweener(5,9)
+
+def combine_distinct_and_sort(list1,list2):
+    a = [char for char in list1 if char not in list2]
+    b = [char for char in list2 if char not in list1]
+    joined = a + b
+    output = sorted(joined)
+    return output
+combine_distinct_and_sort([3,3,5],[1,4,5])
+
+def can_spell_with(target_word,letter_word):
+    letter_word_list = list(letter_word)
+    letter_word_set = list(set(letter_word_list))
+    letter_lower = [x.lower() for x in letter_word_set]
+    target_lower = [x.lower() for x in target_word]
+    output = zip(target_lower,letter_lower)
+    print([x for x in output])
+
+def create_divisors_dict(divisors,upper,lower):
+    d = dict((x,[]) for x in divisors)
+    span = range(upper,lower)
+    for key,val in dict:
+        for x in span:
+            if val % key == 0:
+                d.update({key:val})
+    print(d)
+
+# create_divisors_dict([5,4,5],5,9)
+def alphabetized(s):
+    s = s.split()
+    listToStr = ''.join([str(elem) for elem in s])
+    output = sorted(listToStr)
+    print(output) 
+alphabetized("Welcome to the jungle")
