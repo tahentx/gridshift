76877a6f61ecf39d25a1bad8782e5bad900fdf3b
Todd Hendricks <hendricks.ta@gmail.com>
2020-03-23 11:19:52 -0700

    finished assessment

--- metis_faculty.py
+++ metis_faculty.py
@@ -5,8 +5,13 @@ fac = open('faculty.csv')
 facReader = csv.reader(fac)
 next(facReader)
 data = list(facReader)
-keys = [name[0] for name in data]
-values = [x[1:] for x in data]
-f = dict(zip(keys,values))
-return f
+# keys = [name[0] for name in data]
+# values = [x[1:] for x in data]
+# f = dict(zip(keys,values))
+# return f
 # x = dict.fromkeys([])
+name_list = [n[0] for n in data]
+name_split = [tuple(name.split()) for name in name_list]
+values = [entry[1:] for entry in data]
+faculty_names = dict(zip(name_split,values))
+return faculty_names
