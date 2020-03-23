72a6bd70a0cea5623487b26726e164ddabc349a7
Todd Hendricks <hendricks.ta@gmail.com>
2020-03-23 10:47:19 -0700

    create list list of names from data

--- metis_faculty.py
+++ metis_faculty.py
@@ -1,14 +1,10 @@
 import csv
 from collections import Counter
+import re
 fac = open('faculty.csv')
 facReader = csv.reader(fac)
+next(facReader)
 data = list(facReader)
-
-creds = [x[1] for x in data]
-c = Counter(creds)
-c["PhD/Ph.D."] = c['PhD'] + c['Ph.D.']
-
-
-        
-print(c["PhD/Ph.D."])
-print(c)
+names = [name[0] for name in data]
+print(names)
+# x = dict.fromkeys([])
