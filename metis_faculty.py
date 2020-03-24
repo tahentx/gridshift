0576dbc0aee754654c7aa0d3fcf20d20f71a90ec
Todd Hendricks <hendricks.ta@gmail.com>
2020-03-23 19:59:18 -0700

    break each int down into single values. next step use counter

--- metis_faculty.py
+++ metis_faculty.py
@@ -10,8 +10,15 @@ data = list(facReader)
 # f = dict(zip(keys,values))
 # return f
 # x = dict.fromkeys([])
-name_list = [n[0] for n in data]
-name_split = [tuple(name.split()) for name in name_list]
-values = [entry[1:] for entry in data]
-faculty_names = dict(zip(name_split,values))
-return faculty_names
+# name_list = [n[0] for n in data]
+# name_split = [tuple(name.split()) for name in name_list]
+# values = [entry[1:] for entry in data]
+# faculty_names = dict(zip(name_split,values))
+# return faculty_names
+
+def paint(start,end):
+    jobs = range(start,end + 1)
+    total = [job for job in jobs]
+    broke = [[int(x) for x in str(val)] for val in total]
+    print(broke)
+paint(99,160)
