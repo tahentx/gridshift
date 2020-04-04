6feef5bae623a9e529491fd5ee785214cf0ab9ef
Todd Hendricks <hendricks.ta@gmail.com>
2020-04-04 12:15:58 -0700

    fibonacci sequence

--- euler.py
+++ euler.py
@@ -9,4 +9,18 @@ def euler_one(num):
             factors.append(x)
     total = sum(factors)
     return total
-euler_one(1)
+# euler_one(1)
+
+def Fibonacci(n): 
+    if n<0: 
+        print("Incorrect input") 
+    elif n==1: 
+        return 0 
+    elif n==2: 
+        return 1
+    else: 
+        return Fibonacci(n-1)+Fibonacci(n-2) 
+  
+# Driver Program 
+print(Fibonacci(9)) 
+  
